c31f6ff25e4a4103b627561eea15634551148713
cristianCor19 <cordobac96@gmail.com>
2024-02-27 20:00:45 -0500

    url changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // creacion de conexion con la api
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://backinfotect.onrender.com',
     withCredentials: true
 })
 
