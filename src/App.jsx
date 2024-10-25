2a05b040355796da6a4df6d79a72ca93485afa55
cristianCor19 <cordobac96@gmail.com>
2024-10-24 23:48:03 -0500

    Remove import query

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import HomePage from "./pages/HomePage"
 import Navbar from "./components/Navbar"
