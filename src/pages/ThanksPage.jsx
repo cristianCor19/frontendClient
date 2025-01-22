16959458efae15db524175a62e3f1edd59df9d3d
cristianCor19 <cordobac96@gmail.com>
2025-01-22 16:29:32 -0500

    feat: Improve the page with new style appreciationa and link to social networks

--- src/pages/ThanksPage.jsx
+++ src/pages/ThanksPage.jsx
@@ -1,6 +1,23 @@
+import '../styles/thanksPage.css'
+
 function ThanksPage(){
     return (
-        <div>Gracias por su compra</div>
+        <div className='container-thanks-page'>
+            <section className='message-thanks'>
+
+                <h1>Muchas gracias por su compra!</h1>
+            </section>
+            <section className='social-networks-thanks'>
+                {/* <p>En breve recibirá un correo con los detalles de su compra</p> */}
+                <p>Siguenos  tambien en nuestras redes sociales</p>
+                <div className='networks-icons-thanks'>
+                    <a href=""><i className="fa-brands fa-facebook"></i></a>
+                    <a href=""><i className="fa-brands fa-instagram"></i></a>
+                    <a href=""><i className="fa-brands fa-twitter"></i></a>
+                </div>
+            </section>
+
+        </div>
     )
 }
 
