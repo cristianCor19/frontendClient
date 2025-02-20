ed88d6378b2c8007bfd55b919d0839df191e8f60
cristianCor19 <cordobac96@gmail.com>
2025-02-19 23:18:52 -0500

    feat: Change image format to enhance performance

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ function HomePage () {
             <SliderComponent></SliderComponent>
             <div className="container__favorites">
                 <div className="container__ubicacion">
-                    <img src="/img/favorite1.jpg" alt="" className="ubicacion__img"/>
+                    <img src="/img/favorite1.avif" alt="" className="ubicacion__img"/>
                     <div className="ubicacion__description">
                         <h2 className="ubicacion__title">Visítanos en nuestra ubicación</h2>
                         <a href="" className="ubicacion__link">Sogamoso, Boyacá
@@ -16,7 +16,7 @@ function HomePage () {
                     </div>
                 </div>
                 <div className="container__facebook">
-                    <img src="/img/favorite2.jpg" alt="" className="facebook__img img-favorites"/>
+                    <img src="/img/favorite2.avif" alt="" className="facebook__img img-favorites"/>
                     <div className="facebook__description">
                         <h2 className="facebook__title">Síguenos en Facebook</h2>
                         <a href="" className="facebook__link">Facebook
